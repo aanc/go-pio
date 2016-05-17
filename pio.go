abf5f4f995bd13bbfb6b7bc2b230a8c2ec9edbdb
Adrien Anceau <adrien.anceau@gmail.com>
2016-05-17 16:09:08 +0200

    Implemented command info

--- pio.go
+++ pio.go
@@ -93,7 +93,23 @@ func commandInfo() {
 	check(err)
 
 	username, _ := info.GetString("info", "username")
-	fmt.Println("  Username: " + username)
+	mail, _ := info.GetString("info", "mail")
+	planExpirationDate, _ := info.GetString("info", "plan_expiration_date")
+	diskAvail, _ := info.GetInt64("info", "disk", "avail")
+	diskUsed, _ := info.GetInt64("info", "disk", "used")
+	diskSize, _ := info.GetInt64("info", "disk", "size")
+
+	diskAvail = diskAvail / 1024 / 1024 / 1024
+	diskUsed = diskUsed / 1024 / 1024 / 1024
+	diskSize = diskSize / 1024 / 1024 / 1024
+
+	fmt.Printf(`Account information:
+
+  Username: %s
+  Mail: %s
+  Disk: %.2d/%.2dGB (%.2dGB free)
+  Expiration: %s
+`, username, mail, diskUsed, diskSize, diskAvail, planExpirationDate)
 }
 
 func main() {
